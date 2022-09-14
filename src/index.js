58ab670f397fa1143ba183217a1dad7d01f3deb2
juan-arroyave <juanpabloarroyave1@hotmail.com>
2022-09-14 14:50:47 -0500

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, Switch, Routes, Route } from 'react-router-dom';
 
-
 //Import Axios
 import Axios from 'axios'
 Axios.defaults.baseURL='http://localhost:8000'
